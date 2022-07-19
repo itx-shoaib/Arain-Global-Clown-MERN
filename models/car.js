433d97283fd02bc57a83979131214cdb19285b69
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-19 14:42:13 +0500

    Making car model -backend

---
+++ models/car.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const carSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    seat: {
+        type: Number,
+        required: true
+    },
+    door: {
+        type: Number,
+        required: true
+    },
+    milage: {
+        type: String,
+        required: true
+    },
+    rentperday: {
+        type: Number,
+        required: true
+    },
+    imageurls : [],
+    currentbookings: []
+} , {
+    timestamps : true,
+})
+
+const carModal = mongoose.model('cars',carSchema)
+
+module.exports = carModal
