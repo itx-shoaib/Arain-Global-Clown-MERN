909dfb355e3a933b3b6c98485d400cc7a19235a0
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-19 14:57:29 +0500

    Adding carRoute path in server - backend

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ connectToMongo();
 const app = express()
 const port = 5000
 
+const dbconfig = require('./db')
+const carRoute = require('./routes/carRoute')
+
+app.use(express.json());
+app.use('/api/car' , carRoute)
+
 
 app.listen(port, () => {
   console.log(`Arain Global listening on port ${port}`)
