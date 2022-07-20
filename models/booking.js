365e8e450cf945e7995c715c1c2fc4fc899be2ac
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-20 13:56:28 +0500

    Making booking model

---
+++ models/booking.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = mongoose.Schema({
+    car:{
+        type: String,
+        required:true
+    },
+    carid:{
+        type: String,
+        required: true
+    },
+    fromdate:{
+        type: String,
+        required: true
+    },
+    todate:{
+        type: String,
+        required: true
+    },
+    totaldays:{
+        type: Number,
+        required: true
+    },
+    totalamount:{
+        type: Number,
+        required: true
+    },
+    transactionid:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        required: true,
+        default: "booked"
+    }
+
+
+},{
+    timestamps : true,
+})
+
+const bookingmodal = mongoose.model('bookings',bookingSchema)
+
+module.exports = bookingmodal
