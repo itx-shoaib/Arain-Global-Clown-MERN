074de6b1430710dfe9bd8c644f8faa6de03c8d2e
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-21 17:24:43 +0500

    Changing the name of function register into submit in checkout componenet

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -12,36 +12,36 @@ function Checkout({ match }) {
     const [phone, setphone] = useState();
     const [email, setemail] = useState();
 
-async function register() {
-    const form = {
-        firstname,
-        lastname,
-        address,
-        town,
-        state,
-        phone,
-        email,
-        car,
-        carid
+    async function submit() {
+        const form = {
+            firstname,
+            lastname,
+            address,
+            town,
+            state,
+            phone,
+            email,
+            car,
+            carid
+        }
+
+        try {
+            const data = await (await axios.post(`/api/booking/checkout/${carid}/${car}`), form).data
+            console.log(data)
+            setfirstname('')
+            setlastname('')
+            setaddress('')
+            settown('')
+            setstate('')
+            setphone('')
+            setemail('')
+
+        } catch (error) {
+            console.log(error)
+        }
+
+
     }
-    
-    try {
-        const data = await (await axios.post(`/api/booking/checkout/${carid}/${car}`),form).data
-        console.log(data)
-        setfirstname('')
-        setlastname('')
-        setaddress('')
-        settown('')
-        setstate('')
-        setphone('')
-        setemail('')
-
-    } catch (error) {
-        console.log(error)
-    }
-
-
-}
 
 
     return (
@@ -55,44 +55,44 @@ async function register() {
                 <div className="col-md-5">
                     <h3>Driver Details</h3>
                     <form className='container'>
-                        <div class="form-group">
-                            <label for="firstname">First Name</label>
-                            <input type="text" class="form-control" id="firstname" value={firstname} onChange={(e) => { setfirstname(e.targer.value) }} />
+                        <div className="form-group">
+                            <label htmlFor="firstname">First Name</label>
+                            <input type="text" className="form-control" id="firstname" value={firstname} onChange={(e) => { setfirstname(e.targer.value) }} />
                         </div>
-                        <div class="form-group">
-                            <label for="lastname">Last Name</label>
-                            <input type="text" class="form-control" id="lastname" value={lastname} onChange={(e) => { setlastname(e.targer.value) }} />
+                        <div className="form-group">
+                            <label htmlFor="lastname">Last Name</label>
+                            <input type="text" className="form-control" id="lastname" value={lastname} onChange={(e) => { setlastname(e.targer.value) }} />
                         </div>
                         <h6>Country / Region US</h6>
-                        <div class="form-group">
-                            <label for="streetaddress">Street Address</label>
-                            <input type="text" class="form-control" id="streetaddress" value={address} onChange={(e) => { setaddress(e.target.value) }} />
+                        <div className="form-group">
+                            <label htmlFor="streetaddress">Street Address</label>
+                            <input type="text" className="form-control" id="streetaddress" value={address} onChange={(e) => { setaddress(e.target.value) }} />
                         </div>
-                        <div class="form-group">
-                            <label for="town">Town</label>
-                            <input type="text" class="form-control" id="town" value={town} onChange={(e) => { settown(e.target.value) }} />
+                        <div className="form-group">
+                            <label htmlFor="town">Town</label>
+                            <input type="text" className="form-control" id="town" value={town} onChange={(e) => { settown(e.target.value) }} />
                         </div>
-                        <div class="form-group">
-                            <label for="state">State</label>
-                            <input type="text" class="form-control" id="state" value={state} onChange={(e) => { setstate(e.target.value) }} />
+                        <div className="form-group">
+                            <label htmlFor="state">State</label>
+                            <input type="text" className="form-control" id="state" value={state} onChange={(e) => { setstate(e.target.value) }} />
                         </div>
-                        <div class="form-group">
-                            <label for="phone">Phone</label>
-                            <input type="tel" class="form-control" id="phone" value={phone} onChange={(e) => { setphone(e.target.value) }} />
+                        <div className="form-group">
+                            <label htmlFor="phone">Phone</label>
+                            <input type="tel" className="form-control" id="phone" value={phone} onChange={(e) => { setphone(e.target.value) }} />
                         </div>
-                        <div class="form-group">
-                            <label for="email">Email Address</label>
-                            <input type="email" class="form-control" id="email" value={email} onChange={(e) => { setemail(e.target.value) }} />
+                        <div className="form-group">
+                            <label htmlFor="email">Email Address</label>
+                            <input type="email" className="form-control" id="email" value={email} onChange={(e) => { setemail(e.target.value) }} />
                         </div>
-                        <div class="form-group">
-                            <label for="car">Car name</label>
-                            <input type="text" class="form-control" id="car" value={car} />
+                        <div className="form-group">
+                            <label htmlFor="car">Car name</label>
+                            <input type="text" className="form-control" id="car" value={car} />
                         </div>
-                        <div class="form-group">
-                            <label for="carid">Car id</label>
-                            <input type="text" class="form-control" id="carid" value={carid} />
+                        <div className="form-group">
+                            <label htmlFor="carid">Car id</label>
+                            <input type="text" className="form-control" id="carid" value={carid} />
                         </div>
-                        <button class="btn btn-primary" onClick={register}>Place Order</button>
+                        <button className="btn btn-primary" onClick={submit}>Place Order</button>
                     </form>
 
                 </div>
