6d3b1435d107b45a82037d7c23c91333bb807398
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-21 16:44:09 +0500

    Making environment for checkout api to integrate

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from "react-router-dom";
 
-function Checkout() {
+function Checkout({ match }) {
+    const { carid, car } = useParams();
+    const [firstname, setfirstname] = useState();
+    const [lastname, setlastname] = useState();
+    const [address, setaddress] = useState();
+    const [town, settown] = useState();
+    const [state, setstate] = useState();
+    const [phone, setphone] = useState();
+    const [email, setemail] = useState();
+
     return (
         <div className='container'>
             <h1>Checkout</h1>
             <div className="row">
-                
+
                 <div className="col-md-5">
 
                 </div>
@@ -14,32 +25,40 @@ function Checkout() {
                     <form className='container'>
                         <div class="form-group">
                             <label for="firstname">First Name</label>
-                            <input type="text" class="form-control" id="firstname" />
+                            <input type="text" class="form-control" id="firstname" value={firstname} onChange={(e) => { setfirstname(e.targer.value) }} />
                         </div>
                         <div class="form-group">
                             <label for="lastname">Last Name</label>
-                            <input type="text" class="form-control" id="lastname" />
+                            <input type="text" class="form-control" id="lastname" value={lastname} onChange={(e) => { setlastname(e.targer.value) }} />
                         </div>
                         <h6>Country / Region US</h6>
                         <div class="form-group">
                             <label for="streetaddress">Street Address</label>
-                            <input type="text" class="form-control" id="streetaddress" />
+                            <input type="text" class="form-control" id="streetaddress" value={address} onChange={(e) => { setaddress(e.target.value) }} />
                         </div>
                         <div class="form-group">
                             <label for="town">Town</label>
-                            <input type="text" class="form-control" id="town" />
+                            <input type="text" class="form-control" id="town" value={town} onChange={(e) => { settown(e.target.value) }} />
                         </div>
                         <div class="form-group">
                             <label for="state">State</label>
-                            <input type="text" class="form-control" id="state" />
+                            <input type="text" class="form-control" id="state" value={state} onChange={(e) => { setstate(e.target.value) }} />
                         </div>
                         <div class="form-group">
                             <label for="phone">Phone</label>
-                            <input type="tel" class="form-control" id="phone" />
+                            <input type="tel" class="form-control" id="phone" value={phone} onChange={(e) => { setphone(e.target.value) }} />
                         </div>
                         <div class="form-group">
                             <label for="email">Email Address</label>
-                            <input type="email" class="form-control" id="email" />
+                            <input type="email" class="form-control" id="email" value={email} onChange={(e) => { setemail(e.target.value) }} />
+                        </div>
+                        <div class="form-group">
+                            <label for="car">Car name</label>
+                            <input type="text" class="form-control" id="car" value={car} />
+                        </div>
+                        <div class="form-group">
+                            <label for="carid">Car id</label>
+                            <input type="text" class="form-control" id="carid" value={carid} />
                         </div>
                         <button type="submit" class="btn btn-primary">Place Order</button>
                     </form>
