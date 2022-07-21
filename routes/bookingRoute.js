0261a8bdeac8a0886309c72032c1de1390e7fdec
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-21 13:26:51 +0500

    API path for bookcar is working.Deleting the basic logic and commenting out the real logic

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -5,38 +5,28 @@ const Booking = require('../models/booking');
 const Car = require('../models/car')
 
 // Booking a car by POST method PATH: /api/booking/bookcar
+// STATUS: PATH IS WORKING
 router.post('/bookcar',async(req,res)=>{
 
-    // This logic is for project
-    // const {cars,fromdate,todate,totalamount,totaldays} = req.body
+    const {cars,fromdate,todate,totalamount,totaldays} = req.body
 
-    // try {
-        
-    //     const newBooking = await Booking({
-    //         car:cars.name,
-    //         carid:cars._id,
-    //         fromdate:fromdate,
-    //         todate:todate,
-    //         totalamount:totalamount,
-    //         totaldays:totaldays
-    //     })
-    
-    //     const booking = await newBooking.save()
-    //     const cartemp = await Car.findOne({_id:car._id});
-    //     cartemp.currentbookings.push({bookingid:booking._id, fromdate:fromdate, todate:todate, car:car.name, carid: car._id})
-    //     await cartemp.save();
-    //     res.send("Your car has been booked successfully")
-
-    // } catch (error) {
-    //     return res.status(400).json({error})
-    // }
-
-    // This logic is to check API through thunderclient.
     try {
-        const newBooking = await Booking(req.body)
-        await newBooking.save();
-        res.send("Your car has been booked successfully")
         
+        const newBooking = await Booking({
+            car:cars.name,
+            carid:cars._id,
+            fromdate:fromdate,
+            todate:todate,
+            totalamount:totalamount,
+            totaldays:totaldays
+        })
+    
+        const booking = await newBooking.save()
+        const cartemp = await Car.findOne({_id:car._id});
+        cartemp.currentbookings.push({bookingid:booking._id, fromdate:fromdate, todate:todate, car:car.name, carid: car._id})
+        await cartemp.save();
+        res.send("Your car has been booked successfully")
+
     } catch (error) {
         return res.status(400).json({error})
     }
