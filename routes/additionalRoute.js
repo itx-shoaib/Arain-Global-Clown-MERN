5ec50ded910ca0362f70af262532e4e0cf7cde79
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-22 15:59:45 +0500

    Updating addinformation API and making it working

--- routes/additionalRoute.js
+++ routes/additionalRoute.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const Additional = require('../models/additional')
 
-router.post('/addinformation');
+// Router 1: Adding a addon by POST method PATH: /api/addon/addinformation
+// Status: Path is working
+router.post('/addinformation',async(req,res)=>{
+    try {
+        const additional = await Additional(req.body)
+        await additional.save()
+        res.send("Your addon has been added successfully")
+        
+    } catch (error) {
+        return res.status(400).json({error})
+    }
+});
 
 module.exports = router;
