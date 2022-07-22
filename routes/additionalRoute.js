65e82fc0e0f9aec096f23e62f0bf55fb5f1edf2b
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-22 15:45:08 +0500

    Making a basic api for adding additionals

---
+++ routes/additionalRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const Additional = require('../models/additional')
+
+router.post('/addinformation');
+
+module.exports = router;
