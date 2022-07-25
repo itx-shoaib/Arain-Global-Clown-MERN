212f2139f70d618e37648095c77a847d062f8261
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-25 14:18:56 +0500

    Adding app.use path and requirement in server.js

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ const port = 5000
 const carRoute = require('./routes/carRoute')
 const bookRoute = require('./routes/bookingRoute')
 const additionalRoute = require('./routes/additionalRoute')
+const orderRoute = require('./routes/orderRoute')
 
 app.use(express.json())
 app.use('/api/car' , carRoute);
 app.use('/api/booking' , bookRoute);
 app.use('/api/addon' , additionalRoute);
+app.use('/api/order' , orderRoute);
 
 
 app.listen(port, () => {
