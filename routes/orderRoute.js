d81ff01be2b475f451bcd481d3ffb9e5d90f658b
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-25 14:22:52 +0500

    Path test

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// Route:1 
-router.post('',(req,res)=>{
+// Route:1 Getting order dates in order Model by POST method : /api/order/takeorderdate
+// STATUS: Path is Working
+router.post('/takeorderdate',(req,res)=>{
     res.send("Hello")
 });
 
