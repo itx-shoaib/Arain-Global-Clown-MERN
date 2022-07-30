e1717c54c3e365faec364341960a0b399559ddab
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-30 23:03:23 +0500

    Upgradation

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -55,13 +55,10 @@ router.post('/addtocartdetail',async(req,res)=>{
     try {
         const additional = await Additional.findOne({_id:id})
         const order = await Order.findOne({_id:orderid})
-        // if(){
-
-        // }
         order.cartdetail.push({additionalid:id,title:additional.title, price:additional.price})
         await order.save();
         res.send(order)
-        
+
     } catch (error) {
         return res.status(400).json({error})
     }
